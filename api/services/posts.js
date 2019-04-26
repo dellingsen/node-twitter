a8efa59c7bbce2a20dbb24bd2a6860d399f333c1
dan.ellingsen <drellingsen@gmail.com>
2019-04-26 08:38:23 -0500

    update bot

--- api/services/posts.js
+++ api/services/posts.js
@@ -3,19 +3,135 @@ var config = require('./../../config.js');
 
 var Twitter = new twit(config.twitter);
 
+// Called via automated Bot when server starts
+exports.stream = function(callback)
+{
+	//console.log("stream is on - filtering tweets by location...")
+	// A user stream
+	//let stream = Twitter.stream('user');
+	//let stream = Twitter.stream('public');
+	//let stream = Twitter.stream('site');
+
+	//bbox = left,bottom,right,top
+	//bbox = min Longitude , min Latitude , max Longitude , max Latitude
+	//http://bboxfinder.com
+        //For example track=twitter&locations=-122.75,36.8,-121.75,37.8 would match any Tweets containing the term Twitter (even non-geo Tweets) 
+
+	let sanfran = [ '-122.75', '36.8', '-121.75', '37.8' ]
+	let minneapolis = [ '-93.26', '45.0', '-92.26', '46.0' ]
+	let melbourneBeach = [ '-80.572958', '28.017742', '-80.426788' ,'28.076297' ]
+	//http://boundingbox.klokantech.com/ (broader area)
+	let capeToVeroBeach = [ '-80.6354', '27.7019', '-80.3622', '28.5169']
+        let jacksonvilleToMiami = [-81.4519,25.721,-79.8589,30.656];
+        let daytonaToLauderdale = ['-81.1237', '26.0488', '-79.9042', '29.5658'];
+        let florida = ['-84.6', '24.24', '-79.44', '30.69'];
+
+	//let stream = Twitter.stream('statuses/filter', {'locations': minneapolis})
+	//Twitter API only uses OR condition, so you need to manually track keywords from location
+	//let stream = Twitter.stream('statuses/filter', { 'locations': daytonaToLauderdale, track: ["#shark","shark"] })
+	//like this...
+	let filterStream = Twitter.stream('statuses/filter', { 'locations': florida})
+	//send results to tweetEvent() function
+	filterStream.on('tweet', tweetEvent);
+
+	// Twitter user streams were retired in 8/23/2018 so this code will no longer work
+	//let userStream = Twitter.stream('just_a_bot');
+	// When someone follows the user this bot is configured for
+	//userStream.on('follow', followEvent);
+}
+
+// called from stream() when we are tracking a tweet event
+function tweetEvent(tweet) {
+    let trackingWord1 = 'shark attack';
+    let trackingWord2 = '#shark';
+    let trackingWord3 = '#sharkattack';
+    let trackingWord4 = 'shark spotted'; 
+    let filters = ['shark attack', '#shark', '#sharkattack', 'shark spotted'];
+
+    var reply_to = tweet.in_reply_to_screen_name;
+     // Check to see if this was, in fact, a reply to you
+	  var date = tweet.created_at;
+    var name = tweet.user.name;
+    var location = tweet.user.location;
+    var screenname = tweet.user.screen_name;
+    var tweetText = tweet.text;
+    //Look for tweet id so we can pass to retweet!
+    //console.log("tweetEvent - tweet: ");
+    //console.log(tweet);
+    /*
+    console.log("tweetEvent - date: " + date);
+    console.log("tweetEvent - name: " + name);
+    console.log("tweetEvent - screenname: " + screenname);
+    console.log("tweetEvent - location: " + location);
+    console.log("tweetEvent - text: " + tweetText);
+    */
+
+    for (const filter of filters) {
+      if (tweetText.search(filter) !== -1) {
+        console.log("FOUND tweet containing search word: ");
+        Twitter.post('statuses/update', { status: tweetText }, function(err, response) {
+	  console.log('Posted new tweet now....');
+	  console.log(response);
+  	});
+      }
+    }
+
+}
+
+
+function retweet(tweetText, retweetId, callback) {
+
+  console.log("Trying to Retweet now...");
+  Twitter.post('statuses/retweet/:id', { id: retweetId  }, function(err, response) {
+      if (response) {
+          console.log('Retweeted success - tweet id: ' + retweetId);
+          console.log(response)
+          callback("success")
+      }
+      else if (err) {
+          console.log('Error trying to reweet: ' + retweetId);
+          callback("error")
+      }
+  });
+
+}
+
+function followEvent(event) {
+
+  var name = event.source.name;
+  var screenName = event.source.screen_name;
+  console.log("followEvent");
+  console.log('I was followed by: ' + name + ' ' + screenName);
+
+  // post back tweet to that user
+  Twitter.post('statuses/update', { status: "@"+screenName + " thanks for the follow!" },
+	function(err, response) {
+	  if (response) {
+	    console.log('Tweeted!!!');
+	  }
+	  // if there was an error while tweeting
+	  if (err) {
+	    console.log('Something went wrong while TWEETING...');
+	    console.log(err)
+	  }
+  });
+
+}
+
+// Called via REST API
 // find latest tweet according the query 'q' in params
 exports.getTweetsSoft = function(paramArray, callback) {
 	var searchParams = '';
 	for (var i=0; i<paramArray.length; i++) {
 		searchParams += paramArray[i] + ",";
 	}
-	
+
     var params = {
         q: searchParams,  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     }
-	
+
     Twitter.get('search/tweets', params, function(err, data) {
       // if there no errors
         if (!err) {
@@ -32,6 +148,7 @@ exports.getTweetsSoft = function(paramArray, callback) {
     });
 }
 
+// Called via REST API
 exports.getTweetsHard = function(callback) {
     var params = {
         q: '#MNHockey, #MNWild',  // REQUIRED
@@ -53,114 +170,6 @@ exports.getTweetsHard = function(callback) {
         }
     });
 }
-
-exports.stream = function(callback) 
-{
-	console.log("Looking for tweets by location or keywords")
-	// A user stream
-	//var stream = Twitter.stream('user');
-	//var stream = Twitter.stream('public');
-	//var stream = Twitter.stream('site');
-	var sanfran = [ '-122.75', '36.8', '-121.75', '37.8' ]
-	var minneapolis = [ '-93.26', '45.0', '-92.26', '46.0' ]
-	var stream = Twitter.stream('statuses/filter', { track: "nodejs" })
-
-	// When someone follows the user
-	// In this callback we can see the name and screen name
-	//stream.on('follow', followEvent);
-	stream.on('tweet', tweetEvent);
-	
-	// A geo location
-	//stream.on('tweet', tweetEvent);
-	
-}
-
-function followEvent(event) {
-
-  var name = event.source.name;
-  var screenName = event.source.screen_name;
-  console.log("followEvent");
-  console.log('I was followed by: ' + name + ' ' + screenName);
-  
-  // post back tweet to that user
-  Twitter.post('statuses/update', { status: "@"+screenName + " thanks for the follow!" }, 
-	function(err, response) {
-		if (response) {
-			console.log('Tweeted!!!');
-		}
-		// if there was an error while tweeting
-		if (err) {
-			console.log('Something went wrong while TWEETING...');
-			console.log(err)
-		}
-  });
-	
-}
-
-function tweetEvent(tweet) {
-	
-    var reply_to = tweet.in_reply_to_screen_name;
-    // Check to see if this was, in fact, a reply to you
-	var date = tweet.created_at;
-    var name = tweet.user.name;
-    var location = tweet.user.location;
-    var screenname = tweet.user.screen_name;
-    var tweetText = tweet.text;
-    console.log("tweetEvent - date: " + date);
-    console.log("tweetEvent - name: " + name);
-    console.log("tweetEvent - screenname: " + screenname);
-    console.log("tweetEvent - location: " + location);
-    console.log("tweetEvent - text: " + tweetText);
-
-
-	var params = {screen_name: screenname};
-	Twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  if (!error) {
-		console.log("TIMELINE TWEETS:");
-		console.log(tweets.length);
-		if (tweets[0].retweeted_status) {
-			console.log("Retweeted timeline user location:")
-			console.log(tweets[0].retweeted_status.user.location);
-		}
-	  }
-	});
-
-  
-}
-
-exports.retweet = function(callback) {
-    var params = {
-        q: '#angularjs, #nodejs',  // REQUIRED
-        result_type: 'recent',
-        lang: 'en'
-    }
-    Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
-        if (!err && data.statuses.length > 0) {
-          // grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-					callback(response)
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-					callback(err)
-                }
-            });
-        }
-        // if unable to Search a tweet
-        else {
-          console.log('Didnt find any tweets to retweet');
-        }
-    });
-}
-
 
 // find a random tweet and 'favorite' it
 exports.favoriteTweet = function(callback) {
@@ -198,18 +207,21 @@ exports.favoriteTweet = function(callback) {
 exports.createTweet = function(body, callback) {
 
     var tweet = body.status;
-	
-	Twitter.post('statuses/update', { status: tweet }, function(err, response) {
-		if (response) {
-			console.log('Tweeted!!!');
-		}
-		// if there was an error while tweeting
-		if (err) {
-			console.log('Something went wrong while TWEETING...');
-			console.log(err)
-		}
-	});
+    Twitter.post('statuses/update', { status: tweet }, function(err, response) {
+	if (response) {
+		console.log('Tweeted!!!');
+	}
+	// if there was an error while tweeting
+	if (err) {
+		console.log('Something went wrong while TWEETING...');
+		console.log(err)
+	}
+    });
+}
 
+function post (content) {
+  twitter.post('statuses/update', { status: content }, function(err, data, response) {
+  })
 }
 
 // function to generate a random tweet tweet
@@ -217,3 +229,36 @@ function ranDom (arr) {
   var index = Math.floor(Math.random()*arr.length);
   return arr[index];
 };
+
+
+/////////////////////////// Search Examples ///////////////////////////
+/*
+var musicParties, 
+        interactiveParties, 
+        filmParties;
+
+Twitter.get('search/tweets', { q: 'SXSW music party ', count: 100 }, function(err, data, response) {
+  musicParties = data.statuses;
+  console.log("MUS " + musicParties[0].text);
+})
+
+Twitter.get('search/tweets', { q: 'SXSW interactive party ', count: 100 }, function(err, data, response) {
+  interactiveParties = data.statuses;
+  console.log("INT " + interactiveParties[0].text);
+})
+
+Twitter.get('search/tweets', { q: 'SXSW film party ', count: 100 }, function(err, data, response) {
+  filmParties = data.statuses;
+  console.log("FLM " + filmParties[0].text);
+})
+
+Twitter.get('search/tweets', { q: 'JavaScript', count: 10 }, gotData);
+
+function gotData(err, data) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+}
+
+*/
--- app.js
+++ app.js
@@ -20,54 +20,37 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for posts
 
+// This is what the res.locals object is for. Setting variables directly on the request object is not supported or documented. 
+// res.locals is guaranteed to hold state over the life of a request.
 // route middleware that will happen on every request
+
 app.use(function(req, res, next) {
   // continue doing what we were doing and go to the route
+  // how to pass variables to next middleware
+  // res.locals.user = req.user;  
   next();
 });
 
-if (botMode) {
-	console.log("Bot is activated");
-	
-    posts.stream(function (out) {
-      console.log(out)
-	})
+if (botMode)
+{
+  console.log("Bot is activated");
 
-	// retweet in every 5 minutes
-	setInterval(function () {
-		posts.stream(function(out) { 
-		   console.log(out)	
-		   }
-	)}, 300000);
-
-	// grab & retweet as soon as program is running...
-    posts.retweet(function (out) {
+  posts.stream(function (out) {
       console.log(out)
-	})
-	
-	// retweet in every 50 minutes
-	setInterval(function () {
-		posts.retweet(function(out) { 
-		   //console.log(out)	
-		   }
-	)}, 3000000);
+  })
 
-	// grab & 'favorite' as soon as program is running...
-    posts.favoriteTweet(function (out) {
+  // search tweets every 5 seconds
+  setInterval(function () {
+    posts.stream(function(out) {
       console.log(out)
-	})
-	
-	// 'favorite' a tweet in every 60 minutes
-	setInterval(function () {
-		posts.favoriteTweet(function(out) { 
-		   //console.log(out)	
-		   }
-	)}, 3600000);
+   }
+  )}, 5000);
+
 }
 
-// Define application routes
 
-//Sample Twitter REST API
+//Define application routes
+//Twitter REST API
 
 // Create endpoint /posts for POST
 app.post('/api/posts', function(req, res) {
@@ -83,7 +66,6 @@ app.get('/api/posts', function(req, res) {
   })
 })
 
-// Create endpoint /posts for GET
 app.get('/api/posts2', function(req, res) {
   console.log(req.query);
   console.log(req.query.search);
@@ -99,9 +81,52 @@ app.get('/api/posts/:postid', function(req, res) {
   })
 })
 
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+})
+
 //Create Http Server to listen for requests to routes
-var server = app.listen(4000, function () {
+let server = app.listen(45000, function () {
   console.log('Listening on port', server.address().port)
 })
+
+server.on('error', onError);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    console.error('Connection was reset!?!?');
+    console.error(error);
+    throw error;
+  }
+
+  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    case 'ECONNRESET':
+      console.error(bind + ' connection was reset!?!?');
+      console.error(error);
+      //process.exit(1);
+      break;
+    default:
+      console.error('Default error condition was captured:');
+      console.error(error);
+      //throw error;
+  }
+}
 
 module.exports = app;
